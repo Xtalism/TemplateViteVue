db91af08845c2bb4723a4096cfeb9f977870f4b5
pinaplehits <pinaplehits@outlook.com>
2024-05-22 01:57:43 +0000

    chore: Update Vuetify import in main.js
    This commit updates the import statement for Vuetify components and directives in the `main.js` file. It replaces the wildcard import with specific imports from the `vuetify/components` and `vuetify/directives` modules. This improves code organization and reduces unnecessary imports.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-// Import styles
 import './style.css'
 import 'vuetify/styles'
-
-// Import Vue and Vuetify components
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-// Import main App component and router
+import * as vuetifyComponents from 'vuetify/components'
+import * as vuetifyDirectives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
 
-// Create Vuetify instance
-const vuetify = createVuetify({ components, directives })
-
-// Create and mount the app
-createApp(App).use(vuetify).use(router).mount('#app')
+createApp(App)
+  .use(
+    createVuetify({
+      components: vuetifyComponents,
+      directives: vuetifyDirectives
+    })
+  )
+  .use(router)
+  .mount('#app')
